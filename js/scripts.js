e123cd171025492bb0e807a791cb343dbb1cc90b
3lder7 <eldergalvao07@gmail.com>
2025-02-17 13:17:43 -0300

    ✨ Adicionar funcionalidade para enviar comentários e exibi-los na página

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,23 @@ function abrirFormComentario(button){//trabalhando com parentElement e nextEleme
         comentarioForm.style.display = 'none';
     }//if else para mostrar e esconder o formulário de comentário ao clicar no botão
 }
+
+function enviarComentario(button) {
+    const comentarioForm = button.parentElement;
+    const textarea = comentarioForm.querySelector('textarea');
+    const comentarioTexto = textarea.value.trim();
+
+    if (comentarioTexto !== '') {
+        const comentarioDiv = document.createElement('div');
+        comentarioDiv.classList.add('comentario');
+        comentarioDiv.textContent = comentarioTexto;
+
+        const post = comentarioForm.parentElement;
+        post.insertBefore(comentarioDiv, comentarioForm);
+
+        textarea.value = '';
+        comentarioForm.style.display = 'none';
+    } else {
+        alert('Por favor, digite um comentário.');
+    }
+}//função para enviar o comentário digitado no formulário para a div de comentários
