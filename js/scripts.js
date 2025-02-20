dae4c018b6aa30a183a4243290c59c1718424731
3lder7 <eldergalvao07@gmail.com>
2025-02-20 10:13:43 -0300

    ♻️ Refatorando a estrutura do projeto: atualizar links CSS, adicionar funcionalidade modal para criar postagens e remover estilos não utilizados

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//JS DE COMENTÁRIOS----------------------------------------------
 function abrirFormComentario(button){//trabalhando com parentElement e nextElementSibling pra facilitar  uso de vários botões
     const comentarioForm = button.parentElement.nextElementSibling;
     if(comentarioForm.style.display === 'none'){
@@ -10,3 +11,23 @@ function abrirFormComentario(button){//trabalhando com parentElement e nextEleme
 function enviarComentario(button) {
     const comentarioForm = button.parentElement;//criação variável pra pegar o elemento pai do botão clicado
 }//função para enviar o comentário digitado no formulário para a div de comentários
+
+//JS DE POSTS/MODAL---------------------------------------------------
+function abrirModal() {
+    document.getElementById('modalPost').style.display = 'block';
+}
+
+function fecharModal() {
+    document.getElementById('modalPost').style.display = 'none';
+}
+
+function criarPost() {
+    var conteudo = document.getElementById('novoPostConteudo').value;
+    if (conteudo) {
+        // lógica para criar um novo post
+        alert('Post criado: ' + conteudo);
+        fecharModal();
+    } else {
+        alert('Por favor, digite o conteúdo do post.');
+    }
+}
